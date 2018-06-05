23daa7656461a77eec2bb627219450de9de34fc5
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-06-04 19:22:48 -0600

    particles

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var typed = new Typed('#intro', {
+    strings: ["Hello, I'm Sam Pritchard."],
+    typeSpeed: 40,
+});
+
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  
+
+
+
+
