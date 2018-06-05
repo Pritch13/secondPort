bffa0050e002da2879f8ad9f8c96263700bf05f3
Sam Pritchard <sam@Sams-MacBook-Pro.local>
2018-06-04 22:19:57 -0600

    scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,40 @@ particlesJS.load('particles-js', 'assets/particles.json', function() {
   $(document).ready(function(){
     $('.parallax').parallax();
   });
-      
+
+
+    window.sr = ScrollReveal();
+
+    sr.reveal('.first', {
+        duration: 1000,
+        origin: 'left',
+    });  
+  
+    sr.reveal('.about', {
+        duration: 1000,
+        origin: 'top',
+    });  
+
+    sr.reveal('p', {
+        duration: 1000,
+        origin: 'right',
+        distance: '300px'
+    }); 
+
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
 
   
 
